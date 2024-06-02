50175e6a2d066c71d9d7327c2d9b76bc755aa349
Fitrah Maulana <fitrahmaulana111@gmail.com>
2024-06-03 00:04:12 +0700

    menghapus desktop.ini

--- tes.py
+++ tes.py
@@ -1,16 +1,78 @@
-from ultralytics import YOLO
+import os
+import threading
+import time
 
-# Load a model
-model = YOLO("best.pt")  # pretrained YOLOv8n model
+global files, number_files, excluding, excluded, stop
 
-# Run batched inference on a list of images
-results = model.predict("./static/uploads/carPark.mp4_000031375.png", workers=0)  # return a list of Results objects
+files = []
+number_files = 0
+excluding = 0
+excluded = 0
+stop = False
+    
+def print_run():
+    while stop == False:
+        print(f'Escaneando.', end='\r')
+        time.sleep(1)
+        print(f'             ', end='\r')
+        print(f'Escaneando..', end='\r')        
+        time.sleep(1)
+        print(f'             ', end='\r')
+        print(f'Escaneando...', end='\r')
+        time.sleep(1)
+        print(f'             ', end='\r')
 
-# Process results list
-for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-    result.show()  # display to screen
+def search_for_files(path, searched_file):
+    
+    global files, number_files, excluding, stop
+
+    for (dirpath, dirnames, filenames) in os.walk(path):        
+        for file in filenames:
+            if file == searched_file:
+                files.append({"path": dirpath,
+                              "file": file})
+                excluding = excluding + 1
+        number_files = number_files + len(filenames)
+
+    stop = True
+
+def confirm_exluding():
+
+    global files, excluded
+
+    print(f'>Serão excluidos {excluding} arquivos.')
+    print(f'>Digite "Yes" para confirmar exclusão...')
+
+    answer = input()
+
+    if answer != 'Yes':
+        print(f'>Cancelado!')
+
+    else:
+        for file in files:
+            full_file = file["path"] + '/' + file["file"]
+            os.remove(full_file)
+            print(f'>>>Total de arquivos excluído: { full_file }')
+            excluded = excluded + 1
+
+    print(f'>>>Arquivos excluídos: { excluded }')    
+
+print('>>>Esse programa procura arquivos para deletar de acordo com o nome')        
+print('>Qual directório deseja escanear?')
+
+directory = input()
+
+print('>Qual o nome do arquivo?')
+
+filename = input()
+
+threading.Thread(target=print_run).start()
+
+search_for_files(directory, filename)
+
+print(f'>Total de arquivos escaneados: { number_files }')
+
+if len(files) > 0:
+    confirm_exluding()
+else:
+    print(f'>>>Nenhum arquivo encontrado!')
